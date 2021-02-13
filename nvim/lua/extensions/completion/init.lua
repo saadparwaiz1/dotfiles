330e791ee06a25e35544bb4c0b14a4f40dc4a563
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-02-13 14:12:17 +0000

    Dot File Backup 10

--- nvim/lua/extensions/completion/init.lua
+++ nvim/lua/extensions/completion/init.lua
@@ -1,15 +1,16 @@
 vim.cmd('packadd! nvim-compe')
 
 require('compe').setup {
-	enabled = true;
+	enabled = true,
 	source = {
-		path = true;
-		spell = true;
-		vsnip = true;
-		buffer = true;
-		nvim_lsp = true;
-		nvim_lua = true;
+		path = true,
+		spell = true,
+		vsnip = true,
+		buffer = true,
+		nvim_lsp = true,
+		nvim_lua = true,
 	}
 }
 
 vim.api.nvim_set_keymap('i', '<CR>', 'compe#confirm("<CR>")', {noremap = true, silent = true, expr = true})
+vim.api.nvim_set_keymap('i', '<C-e>', 'compe#close("<C-e>")', {noremap = true, silent = true, expr = true})
